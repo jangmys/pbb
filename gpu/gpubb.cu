4d37a166ae92b587f6eb02b2f56808b488318e8f
GMYS Jan <jan.gmys@inria.fr>
2023-02-09 14:25:07 +0100

    gpu

--- gpu/gpubb.cu
+++ gpu/gpubb.cu
@@ -35,8 +35,8 @@ gpubb::gpubb(pbab * _pbb)
         // bound=bd;
     }
 
-    initialUB = INT_MAX;
-    pbb->sltn->getBest(initialUB);
+    // initialUB = INT_MAX;
+    initialUB = pbb->best_found.getBest();
 
 	FILE_LOG(logINFO) << "GPU with nbIVM:\t" << nbIVM;
 	FILE_LOG(logINFO) << "Initial UB:\t" << initialUB;
@@ -86,7 +86,7 @@ gpubb::initFullInterval()
 {
     if (firstbound) {
         int best = INT_MAX;
-        pbb->sltn->getBest(best);
+        pbb->best_found->getBest(best);
 		FILE_LOG(logINFO) << "Init Full : Bound Root with UB:\t" << best;
 		FILE_LOG(logINFO) << "Init Full : size:\t" << size << " " << nbMachines_h ;
 		FILE_LOG(logINFO) << "Init Full : Root :\t" << pbb->best_found;
@@ -168,7 +168,7 @@ gpubb::launchBBkernel(const int NN)
 {
     int best = INT_MAX;
 
-    pbb->sltn->getBest(best);
+    pbb->best_found->getBest(best);
 
     gpuErrchk(cudaMemset(counter_d, 0, 6 * sizeof(unsigned int)));
     gpuErrchk(cudaMemset(flagLeaf, 0, nbIVM * sizeof(int)));
@@ -255,7 +255,7 @@ gpubb::next()
     int iter = 0;
     int best = INT_MAX;
 
-    pbb->sltn->getBest(best);
+    pbb->best_found->getBest(best);
 
     int nbsteals = 0;
 	localFoundNew=false;
@@ -541,12 +541,12 @@ gpubb::boundLeaves(bool reached, int& best)
 
             if (update) {
                 best = cost;
-                pbb->sltn->update(schedule_h+k*size,cost);
+                pbb->best_found->update(schedule_h+k*size,cost);
 
 				localFoundNew = true;
                 pbb->best_found.foundAtLeastOneSolution.store(true);
 
-                FILE_LOG(logINFO) << "GPUBB found " << *(pbb->sltn);
+                FILE_LOG(logINFO) << "GPUBB found " << pbb->best_found;
 
                 newUB = true;
             }
@@ -1216,7 +1216,7 @@ gpubb::initFromFac(const int nbint, const int* ids, int*pos, int* end)
 
     if (firstbound) {
         int best = INT_MAX;
-        pbb->sltn->getBest(best);
+        pbb->best_found->getBest(best);
 
 		FILE_LOG(logINFO) << "Init intervals: Bound Root with UB:\t" << best;
 		FILE_LOG(logINFO) << "Init intervals: Root:\t" << pbb->best_found;
@@ -1289,7 +1289,7 @@ int gpubb::getDeepSubproblem(int *ret, const int N){
 	int gbest;
 	int *bestsol;
 	cudaMallocManaged(&bestsol,size*sizeof(int));
-    pbb->sltn->getBestSolution(bestsol,gbest);
+    pbb->best_found->getBestSolution(bestsol,gbest);
 
     struct timespec startt,endt;
     clock_gettime(CLOCK_MONOTONIC,&startt);
