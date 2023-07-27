bdb3ae230c6b0176d37884d76648be8d2cfacbb2
Jan Gmys <jan.gmys@univ-lille.fr>
2023-07-27 15:05:59 +0200

    read inifile before args

--- distributed/src/main.cpp
+++ distributed/src/main.cpp
@@ -39,7 +39,6 @@ main(int argc, char ** argv)
 
 // -----------------------Parse args----------------------
 // .. pass ini file with -f <path-to-ini-file> option !
-    arguments::readIniFile();
     arguments::parse_arguments(argc, argv);
 
 // --------------------Set up logging--------------------
