60c6f136b8d58f5478e7937399b143d3b782fa87
Jan Gmys <jan.gmys@univ-lille.fr>
2025-10-24 15:46:02 +0200

    add bound

--- gpu/test.cu
+++ gpu/test.cu
@@ -11,6 +11,50 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+
+void
+initializeBoundFSP(pbab * pbb)
+{
+    int nbJob_h,nbMachines_h;
+
+    // get instance data
+    (pbb->inst->data)->seekg(0);
+    (pbb->inst->data)->clear();
+
+    *(pbb->inst->data) >> nbJob_h;
+    *(pbb->inst->data) >> nbMachines_h;
+    //
+    // somme_h = 0;
+    // for (int i = 1; i < nbMachines_h; i++) somme_h += i;
+    //
+	// nbJobPairs_h = 0;
+	// for (int i = 1; i < nbJob_h; i++) nbJobPairs_h += i;
+    //
+    // // init bound for GPU
+    // allocate_host_bound_tmp();
+    //
+    // for (int i = 0; i < nbMachines_h; i++) {
+    //     fillMachine();
+    //
+    //     for (int j = 0; j < nbJob_h; j++)
+    //         *(pbb->inst->data) >> tempsJob_h[i * nbJob_h + j];
+    //     fillLag();
+    //     fillTabJohnson();
+    //     fillMinTempsArrDep();
+	// 	fillSumPT();
+    // }
+    //
+    // copyH2Dconstant();
+    // free_host_bound_tmp();
+    //
+    // // gpuErrchk(cudaMalloc((void **) &front_d, nbIVM * nbMachines_h * sizeof(int)));
+    // // gpuErrchk(cudaMalloc((void **) &back_d, nbIVM * nbMachines_h * sizeof(int)));
+    //
+    //
+    // bd = std::make_unique<gpu_fsp_bound>(nbJob_h,nbMachines_h,nbIVM);
+}
+
+
 int
 main(int argc, char ** argv)
 {
