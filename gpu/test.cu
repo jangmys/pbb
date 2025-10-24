f60b8b74bc70c5950771322f2d2e350cc096e0bc
Jan Gmys <jan.gmys@univ-lille.fr>
2025-10-24 16:35:00 +0200

    pbab out

--- gpu/test.cu
+++ gpu/test.cu
@@ -1,8 +1,12 @@
+#include <memory>
+
 #include "arguments.h"
 #include "pbab.h"
 // #include "ttime.h"
 // #include "log.h"
 //
+#include "instance_factory.h"
+
 // #include "libbounds.h"
 // #include "gpubb.h"
 #include "gpuerrchk.h"
@@ -11,18 +15,17 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-class pbab;
 
-void initializeBoundFSP(pbab * pbb)
+void initializeBoundFSP(std::shared_ptr<instance_abstract> pbb)
 {
     int nbJob_h,nbMachines_h;
 
     // get instance data
-    (pbb->inst->data)->seekg(0);
-    (pbb->inst->data)->clear();
+    (inst->data)->seekg(0);
+    (inst->data)->clear();
 
-    *(pbb->inst->data) >> nbJob_h;
-    *(pbb->inst->data) >> nbMachines_h;
+    *(inst->data) >> nbJob_h;
+    *(inst->data) >> nbMachines_h;
     //
     // somme_h = 0;
     // for (int i = 1; i < nbMachines_h; i++) somme_h += i;
@@ -32,9 +35,10 @@ void initializeBoundFSP(pbab * pbb)
     //
     // // init bound for GPU
     // allocate_host_bound_tmp();
-    //
+    //pbb_instance
     // for (int i = 0; i < nbMachines_h; i++) {
-    //     fillMachine();
+    //     fillMachine();cmake .. -DGMX_THREAD_MPI=OFF -DGMX_MPI=ON -DBUILD_SHARED_LIBS=OFF -DGMXAPI=OFF -DGMX_INSTALL_NBLIB_API=OFF -DGMX_DOUBLE=ON -DGMX_FFT_LIBRARY=fftw3 -DFFTWF_LIBRARY=/share/libraries/fftw/3.3.10-openmpi.4.1.5/lib/ -DFFTWF_INCLUDE_DIR=/share/libraries/fftw/3.3.10-openmpi.4.1.5/include -DGMX_BLAS_USER=/share/libraries/openblas/0.3.28/gcc/11.3.1/lib/ -DGMX_LAPACK_USER=/share/libraries/openblas/0.3.28/gcc/11.3.1/lib/ -DGMX_CP2K=ON -DCP2K_DIR=/share/applications/cp2k/v2025.2/lib/local/psmp/
+
     //
     //     for (int j = 0; j < nbJob_h; j++)
     //         *(pbb->inst->data) >> tempsJob_h[i * nbJob_h + j];
@@ -61,7 +65,8 @@ main(int argc, char ** argv)
     arguments::parse_arguments(argc, argv);
     std::cout<<"=== solving "<<arguments::problem<<" - instance "<<arguments::inst_name<<std::endl;
 
-    pbab * pbb = new pbab(pbb_instance::make_inst(arguments::problem, arguments::inst_name));
+    std::shared_ptr<instance_abstract> inst=pbb_instance::make_inst(arguments::problem, arguments::inst_name);
+    // pbab * pbb = new pbab(pbb_instance::make_inst(arguments::problem, arguments::inst_name));
 
 #ifdef WITH_GPU
     int dev=1;
