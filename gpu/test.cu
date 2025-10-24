05ec3e78f06974c13cdcb6d409724e3de2eb52e6
Jan Gmys <jan.gmys@univ-lille.fr>
2025-10-24 15:04:28 +0200

    test.cu

--- gpu/test.cu
+++ gpu/test.cu
@@ -16,11 +16,18 @@ main(int argc, char ** argv)
     arguments::parse_arguments(argc, argv);
     std::cout<<"=== solving "<<arguments::problem<<" - instance "<<arguments::inst_name<<std::endl;
 
-
 #ifdef WITH_GPU
+    int dev=1;
+
     //use device 0 by default
-    gpuErrchk(cudaSetDevice(1));
+    gpuErrchk(cudaSetDevice(dev));
     gpuErrchk(cudaFree(0));
+
+    //Device Properties, see https://github.com/NVIDIA/cuda-samples/blob/master/Samples/1_Utilities/deviceQuery/deviceQuery.cpp
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, dev);
+
+    printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
 #endif
 
     return EXIT_SUCCESS;
